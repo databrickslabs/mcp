586d312fd10210900d67d75e9c0fa2f8d8ce9d85
Sid Murching <sid.murching@databricks.com>
2025-04-22 16:55:18 -0700

    gs
    Signed-off-by: Sid Murching <sid.murching@databricks.com>

--- servers/unity_catalog/src/unitycatalog_mcp/__init__.py
+++ servers/unity_catalog/src/unitycatalog_mcp/__init__.py
@@ -1,11 +1,11 @@
 import sys
 from traceback import format_exc
 from unitycatalog_mcp.server import start
-
+from databricks.sdk.config import with_user_agent_extra
 
 def main() -> None:
     import asyncio
-
+    with_user_agent_extra("unitycatalog-mcp")
     asyncio.run(start())
 
 
